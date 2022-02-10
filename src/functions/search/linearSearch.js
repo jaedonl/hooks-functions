5173a6acf3fff37be0fdda76debcf4b60f2e9c5f
Jaedon Lee <31083980+jaedonl@users.noreply.github.com>
2022-02-10 18:29:36 -0500

    -

--- src/functions/search/linearSearch.js
+++ src/functions/search/linearSearch.js
@@ -1,16 +1,29 @@
 export function linearSearch(arr, searchNum) {
-    // for(let i=0; i<arr.length-1; i++) {
-    //     if(element == searchNum) {
-    //         return idx
-    //     } else return -1
-    // }
-
     let left = 0
     let length = arr.length
     let right = length -1
     let position = -1
 
-    dgdsgsd
+    // Run loop from 0 to right
+    for (left=0; left<=right;) {
+
+        
+        if (arr[left] == searchNum) {
+            position = left;
+            break;
+        }
+        if (arr[right] == searchNum) {
+            position = right
+            break;
+        }
+        left++
+        right--
+    }
+
+    //if not found
+    if (position == -1) {
+        return console.log('number not found')
+    }
 }
 
 
---
+++ src/functions/search/linearSearchForloop.js
@@ -0,0 +1,7 @@
+export function linearSearch(arr, searchNum) {
+    for(let i=0; i<arr.length-1; i++) {
+        if(element == searchNum) {
+            return idx
+        } else return -1
+    }
+}
