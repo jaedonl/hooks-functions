82ae52e7e5ee7f7c75d28d8c967606ca337d2ba9
Jaedon Lee <31083980+jaedonl@users.noreply.github.com>
2022-02-10 18:15:55 -0500

    Create linearSearch.js

---
+++ src/functions/search/linearSearch.js
@@ -0,0 +1,16 @@
+export function linearSearch(arr, searchNum) {
+    // for(let i=0; i<arr.length-1; i++) {
+    //     if(element == searchNum) {
+    //         return idx
+    //     } else return -1
+    // }
+
+    let left = 0
+    let length = arr.length
+    let right = length -1
+    let position = -1
+
+    dgdsgsd
+}
+
+
